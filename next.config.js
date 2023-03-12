26c29681745dabebbcf5108375d307c0af55dce6
Angelovski <angelovski.marko@pgmail.com>
2023-03-12 22:22:30 +0100

    Added basePath

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const { client } = require("./config");
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: client,
-  basePath: "/markme",
+  basePath: "/markMe",
 };
