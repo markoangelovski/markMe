209dcf9d7c7add235226e61d50340e725dbed6ce
Angelovski <angelovski.marko@pgmail.com>
2023-05-04 23:00:34 +0200

    Search dropdown z index fix

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,12 @@
 import Link from "next/link";
 import { useState } from "react";
 
+import { client } from "../../../config";
+
 import { Icon } from "../Icons/Icons";
 
-import {
-  getSearchContent,
-  getSearchSuggestions
-} from "../../drivers/backend.driver";
-import { BookmarkIcon, Favicon, FolderIcon } from "../Icons/Icons";
-
-// getSearchContent
+import { getSearchSuggestions } from "../../drivers/backend.driver";
+import { FolderIcon } from "../Icons/Icons";
 
 const Search = () => {
   const [suggestedFolders, setSuggestedFolders] = useState([]);
@@ -23,7 +20,7 @@ const Search = () => {
     setSuggestedBookmarks([]);
   };
 
-  const handleAutoComplete = async e => {
+  const handleAutoComplete = async (e) => {
     const term = e.target.value;
 
     const result =
@@ -46,22 +43,22 @@ const Search = () => {
           <path d="M53.627 49.385L37.795 33.553C40.423 30.046 42 25.709 42 21 42 9.42 32.58 0 21 0S0 9.42 0 21s9.42 21 21 21c4.709 0 9.046-1.577 12.553-4.205l15.832 15.832 4.242-4.242zM2 21C2 10.523 10.523 2 21 2s19 8.523 19 19-8.523 19-19 19S2 31.477 2 21zm33.567 15.093c.178-.172.353-.347.525-.525.146-.151.304-.29.445-.445l14.262 14.262-1.415 1.415-14.261-14.263c.155-.141.293-.299.444-.444z" />
         </svg>
       </span>
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <input
           onInput={handleAutoComplete}
           type="text"
           placeholder="Search"
-          className="block pl-9 pr-4 py-2 w-full bg-gray-900 rounded-sm text-sm placeholder-gray-400 text-white focus:bg-white focus:placeholder-gray-600 focus:text-gray-900 focus:outline-none"
+          className="focus:outline-none block w-full rounded-sm bg-gray-900 py-2 pl-9 pr-4 text-sm text-white placeholder-gray-400 focus:bg-white focus:text-gray-900 focus:placeholder-gray-600"
         />
       </form>
       <button
-        className={`fixed inset-0 h-full w-full cursor-default outline-none   ${
+        className={`outline-none fixed inset-0 h-full w-full cursor-default   ${
           visible ? "block" : "hidden"
         }`}
         onClick={clear}
       ></button>
       <ul
-        className={`absolute bg-white block mt-2.5 py-2 rounded-sm shadow-xl w-full ${
+        className={`absolute z-10 mt-2.5 block w-full rounded-sm bg-white py-2 shadow-xl ${
           visible ? "block" : "hidden"
         }`}
       >
@@ -89,7 +86,13 @@ const Search = () => {
           <li key={i}>
             <a
               className="flex px-4 py-2 text-sm text-gray-800 hover:bg-gray-400 hover:text-white"
-              href={suggestedBookmark.url}
+              href={
+                client +
+                "/stats?url=" +
+                suggestedBookmark.url +
+                "&id=" +
+                suggestedBookmark._id
+              }
               target="_blank"
               title={
                 suggestedBookmark?.parentFolderPath &&
@@ -97,7 +100,7 @@ const Search = () => {
                   " on path: " +
                   suggestedBookmark.parentFolderPath
               }
-              onClick={e => {
+              onClick={(e) => {
                 e.stopPropagation();
                 clear();
               }}
